689f2de28c5d60411f3593b35ed1e38c5344140d
ANTUMN0S <93650430+ANTUMN0S@users.noreply.github.com>
2022-01-15 18:02:45 +0100

    Scripts used on the cluster
    All the scripts used for the tasks performed on the cluster.
    Check step_by_step.txt for execution order and where to get additional files that are required.

---
+++ concatenator.sh
@@ -0,0 +1,5 @@
+# file from ENSEMBL: mart_export.txt
+# file from NCBI: sequence.txt
+# file from GtRNAdb: rn7-tRNAs.txt
+
+cat *.txt > Rnor_r_sno_t_RNA.fa
---
+++ renamer.sh
@@ -0,0 +1,6 @@
+mv SRR9596295.fastq.gz Som_Pol_1.fastq.gz
+mv SRR9596296.fastq.gz Som_Pol_02.fastq.gz
+mv SRR9596300.fastq.gz Som_Pol_03.fastq.gz
+mv SRR9596310.fastq.gz Neur_Pol_01.fastq.gz
+mv SRR9596303.fastq.gz Neur_Pol_02.fastq.gz
+mv SRR9596304.fastq.gz Neur_Pol_03.fastq.gz
